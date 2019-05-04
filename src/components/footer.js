0d9dca5fa63c06efe2cdf18184a6b4d348f7d050
GrusonJulien <50272433+GrusonJulien@users.noreply.github.com>
2019-05-04 20:22:40 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,10 +12,10 @@ const Footer = ({ copyrights }) => (
     ) : (
       <>
         <span className="footerCopyrights">
-          © 2019 Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © 2019 RMB 
         </span>
         <span className="footerCopyrights">
-          Starter created by <a href="https://radoslawkoziel.pl">panr</a>
+          Site créer par JulienG
         </span>
       </>
     )}
